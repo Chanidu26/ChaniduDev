5b1cf87cd93f4174369ed2ac7e6378ad356358ab
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-12-02 23:25:09 +0530

    created structure

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 className='text-5xl'>
+      <h1 className='text-5xl font-bold'>
         Hello-world
       </h1>
     </div>
