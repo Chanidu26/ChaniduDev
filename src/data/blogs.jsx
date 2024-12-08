034e69e01c9f78c34417224c08ae5d6dc773f75b
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-12-08 16:54:16 +0530

    added blog figures

---
+++ src/data/blogs.jsx
@@ -0,0 +1,34 @@
+import blog1 from '../assets/blog1.jpeg'
+import blog2 from '../assets/blog2.jpeg'
+import blog3 from '../assets/blog3.jpeg'
+const blogs = [
+    {
+      id: 1,
+      title: "Field Visit 2024 | Creative Software",
+      date: "13 June 2024",
+      description:
+        "Visited a Exciting Field Visit of Creative Software and getting industry experience",
+      image: blog1, // Replace with your blog image URL
+      tag: "Industry",
+    },
+    {
+      id: 2,
+      title: "Student Volunteer",
+      date: "05 May 2024",
+      description:
+        "Excited a recognized as a one of the Best Volunteers in August 2024 at IEEE Student Branch University of Ruhuna",
+      image: blog2,
+      tag: "Volunteering",
+    },
+    {
+      id: 3,
+      title: "CareerXpo 1.0 2024",
+      date: "05 May 2024",
+      description:
+        "Exciting to be a part of the CareerXpo 1.0 2024 as a Design and Publicity Team Member of the Organizing Commitee",
+      image: blog3,
+      tag: "Events",
+    },
+    // Add more blog entries here
+  ];
+export default blogs;
