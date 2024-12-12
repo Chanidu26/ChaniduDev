4b947c860c0b3330c854ecab73e9e4fce3f37c59
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2024-12-12 21:18:32 +0530

    Update projectcard.jsx

--- src/cards/projectcard.jsx
+++ src/cards/projectcard.jsx
@@ -11,12 +11,12 @@ const Projectcard = ({id,image,title,description,technologies,link}) => {
                 alt={title}
                 className="w-full h-48 object-cover"
               />
-              <div className="p-6">
+              <div className="p-3">
                 <h2 className="text-xl font-bold text-white">
                   {title}
                 </h2>
-                <p className="text-gray-300 mt-2">{description}</p>
-                <div className="mt-4">
+                <p className="text-gray-300 mt-1">{description}</p>
+                <div className="mt-2">
                   <h3 className="font-medium text-white">Technologies:</h3>
                   <div className="flex flex-wrap mt-2">
                     {technologies.map((tech, index) => (
@@ -33,7 +33,7 @@ const Projectcard = ({id,image,title,description,technologies,link}) => {
                   href={link}
                   target="_blank"
                   rel="noopener noreferrer"
-                  className="block text-center mt-6 bg-yellow-300 text-gray-900 py-2 rounded hover:bg-blue-700"
+                  className="block text-center mt-3 bg-yellow-300 text-gray-900 py-2 rounded hover:bg-blue-700"
                 >
                   View Project
                 </a>
