ff05dca0b453429f69e71473d125cddc68baa230
Chanidu Karunarathna <137591542+Chanidu26@users.noreply.github.com>
2025-01-23 13:27:14 +0530

    added project

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -1,30 +1,39 @@
 import project1 from '../assets/project1.png'
 import project2 from '../assets/project2.jpg'
 import project3 from '../assets/project3.png'
+import project4 from '../assets/project4.jpg'
 const projects = [
     {
       id: 1,
       title: "ChaniduDev",
-      description: "Personal Portfolio to showcase my skills and projects",
+      description: "Personal Portfolio website to showcase my skills and projects.",
       technologies: ["React", "Tailwind CSS", "Email.js","Framer Motion","Vercel", "Github Actions"],
       image: project1, 
       link: "https://github.com/Chanidu26/ChaniduDev",
     },
     {
       id: 2,
-      title: "CI CD Pipeline",
+      title: "DevOps Pipeline",
       description: "CI CD Pipeline to automate and Containerize nodejs application and Push to Dockerhub using Github actions.",
-      technologies: ["Docker", "Github actions", "Dockerhub","Nodejs"],
+      technologies: ["Docker", "Github Actions", "Dockerhub","Nodejs"],
       image: project2, 
       link: "https://github.com/Chanidu26/docker-ci-automation-github-actions",
     },
     {
       id: 3,
       title: "Loan Approval Prediction Model",
-      description: "Trained the ML model using SVM and Logistic Regression. Get Accuracy perfromance of 94%(SVM) and 91%(Logistic Regression",
+      description: "ML Model to predict loan approval using SVM and Logistic Regression. Accuracy 94%(SVM), 91%(Logistic Regression) , Deployed to AWS through MLOps.",
       technologies: ["Jupyter Notebook", "Python", "Machine Learning"],
       image: project3, 
       link: "https://github.com/Chanidu26/Loan_Approval_Prediction",
+    },
+    {
+      id: 4,
+      title: "Volunteer Management Database System",
+      description: "Database System to manage volunteers and events efficiently. Created using Database Management Operations",
+      technologies: ["MySQL", "DBMS"],
+      image: project4, 
+      link: "https://github.com/Chanidu26/Volunteer-Management-Database-System-Group-Project-MySQL-",
     },
     
   ]
